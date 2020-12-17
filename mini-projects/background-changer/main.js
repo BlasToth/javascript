f305881116fda32411a2ce77120aeb90238533f9
BlasToth <balazstothlaszlo+github@gmail.com>
2020-12-17 20:41:09 +0100

    add setInterval()

--- mini-projects/background-changer/main.js
+++ mini-projects/background-changer/main.js
@@ -1,9 +1,15 @@
 const btn = document.getElementById('btn');
 
-function randomBgChanger() {
+const randomBgChanger = function () {
     return `hsl(${Math.floor(Math.random() * 360)}, 100%, 50%)`;
 }
 
 btn.addEventListener('click', function() {
     document.body.style.background = randomBgChanger();
 });
+
+// const nyom = function() {
+//     document.body.style.background = randomBgChanger();
+// }
+
+// btn.addEventListener('click', setInterval(nyom, 1000));
