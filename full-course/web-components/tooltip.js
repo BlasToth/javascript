1dcdf0df80c6310152b8d933d434021de0af5f06
BlasToth <balazstothlaszlo+github@gmail.com>
2021-01-11 14:37:15 +0100

    add an attribute

--- full-course/web-components/tooltip.js
+++ full-course/web-components/tooltip.js
@@ -2,9 +2,12 @@ class Tooltip extends HTMLElement {
     constructor() {
         super();
         this._tooltipContainer;
-        
+        this._tooltipText = 'dummy tooltip text';
     }
     connectedCallback(){
+        if (this.hasAttribute('text')) {
+            this._tooltipText = this.getAttribute('text');
+        }
         const tooltipIcon = document.createElement('span');
         tooltipIcon.textContent = ' (?) ';
         tooltipIcon.addEventListener('mouseenter', this._showTooltip.bind(this));
@@ -15,8 +18,8 @@ class Tooltip extends HTMLElement {
 
     _showTooltip() {
         this._tooltipContainer = document.createElement('div');
-        this._tooltipContainer.textContent = 'This is the tooltip text!';
-        this.appendChild(tooltipContainer);
+        this._tooltipContainer.textContent = this._tooltipText;
+        this.appendChild(this._tooltipContainer);
     }
 
     _hideTooltip() {
