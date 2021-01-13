738e7d8033dc532023bc4b3d14c62055c1055b7f
BlasToth <balazstothlaszlo+github@gmail.com>
2021-01-13 10:38:44 +0100

    add some general logic

--- full-course/web-components/basic-modal-component/modal.js
+++ full-course/web-components/basic-modal-component/modal.js
@@ -12,6 +12,8 @@ class Modal extends HTMLElement {
                     height: 100vh;
                     background: rgba(0,0,0,0.75);
                     z-index: 1;
+                    opacity: 0;
+                    pointer-events: none;
                 }
                 #modal {
                     position: fixed;
@@ -24,6 +26,8 @@ class Modal extends HTMLElement {
                     display: flex;
                     flex-direction: column;
                     justify-content: space-between;
+                    opacity: 0;
+                    pointer-events: none;
                 }
                 header {
                     padding: 1rem;
