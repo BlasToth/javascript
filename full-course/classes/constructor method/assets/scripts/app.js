e33a1ee210b4b7d6af78c988c9272dc30b687b08
BlasToth <balazstothlaszlo+github@gmail.com>
2021-01-24 20:51:47 +0100

    add Total Output

--- full-course/classes/constructor method/assets/scripts/app.js
+++ full-course/classes/constructor method/assets/scripts/app.js
@@ -15,6 +15,12 @@ class Product {
 class ShoppingCart {
   items = [];
 
+  addProduct(product) {
+    this.items.push(product);
+    this.totalOutput = `<h2>Total: \$${1}</h2>`;
+
+  }
+
   render() {
     const cartEl = document.createElement('section');
     cartEl.innerHTML = `
@@ -22,6 +28,7 @@ class ShoppingCart {
       <button>Order now!</button>
     `;
     cartEl.className = 'cart';
+    this.totalOutput = cartEl.querySelector('h2');
     return cartEl;
   }
 }
