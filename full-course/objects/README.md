c4e8562250671822e3f5717d5e060269c555e96f
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-09 08:42:52 +0100

    add README.md file

---
+++ full-course/objects/README.md
@@ -0,0 +1,9 @@
+# What I learned?
+# Objects
+- Object Literals
+- Dot Notation vs. Bracket Notation
+- methods
+- THIS (context object) (if it used in the root of the document, it will refer to the global context: window object)
+- arrow functions work differently with the THIS keyword
+(if an arrow function is used, JS does NOT change the value of THIS to the object)
+
--- full-course/objects/objects.js
+++ full-course/objects/objects.js
@@ -3,12 +3,31 @@ let user = {
     age: 35,
     email: "pepitogrillo@gmail.com",
     location: "Meadows",
-    articles: ["Why I need to fast?", "How to get up early?"]
+    articles: ["Why I need to fast?", "How to get up early?"],
+    login: function() { // functions in objects are called METHODS
+        console.log("The user is logged in");
+    },
+    logout: function() {
+        console.log("The user logged out");
+    },
+    logArticles: function() {
+        //console.log(this.articles);
+        console.log('This user has written the following articles: ');
+        this.articles.forEach(blog => {
+            console.log(blog);
+        })
+
+    }
 };
 
+user.logArticles();
 
+user.login();
 //user.age = 30;
 
 console.log(user.age);
 
 console.log(user["email"]);
+
+console.log(typeof user);
+
