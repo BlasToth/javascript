eb31e4f5cfb390edb7b89352105397d88d5e03b0
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-09 08:03:30 +0100

    add objects.js

---
+++ full-course/objects/objects.js
@@ -0,0 +1,14 @@
+let user = {
+    name: "Pepito",
+    age: 35,
+    email: "pepitogrillo@gmail.com",
+    location: "Meadows",
+    articles: ["Why I need to fast?", "How to get up early?"]
+};
+
+
+//user.age = 30;
+
+console.log(user.age);
+
+console.log(user["email"]);
