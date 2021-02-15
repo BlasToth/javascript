841ebca2124c095a77e4d5bb58cc85638ac56761
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-15 09:00:31 +0100

    add console message styling

---
+++ full-course/console/console.js
@@ -0,0 +1,6 @@
+console.info("soy un info");
+console.log("soy un log");
+console.log("soy un warn");
+console.error("soy un error");
+
+console.log("%cAlgo ha ido mal","color: #111;font-size: 32px; background:red; font-weight:bold;")
