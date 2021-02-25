c3140c06ad241d0a40770b2940164af5bec54eef
BlasToth <balazstothlaszlo+github@gmail.com>
2021-02-25 10:02:19 +0100

    add setInterval() to automatically turn down the volume in every X second(s)

--- tutorials/volume-control-youtube/control.js
+++ tutorials/volume-control-youtube/control.js
@@ -1,1 +1,4 @@
-document.getElementsByClassName("video-stream")[0].volume = 0.1;
+setInterval(function () {
+    document.getElementsByClassName("video-stream")[0].volume = 0.006;
+    console.log("Volume control is working");
+    }, 1000);
